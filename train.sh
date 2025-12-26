10463f113575c272dc606e8163db982a8fd70602
Boltzmachine <qiuweikang1999@gmail.com>
2025-12-26 01:13:46 -0500

    tracevla train.sh

--- train.sh
+++ train.sh
@@ -1,17 +1,18 @@
 torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
   --disentangle none \
+  --baseline tracevla \
   --static_ratio 0.8 \
-  --mem_sep 40 \
+  --mem_sep 20 \
   --vla_path openvla/openvla-7b \
   --data_root_dir dataset/ \
-  --dataset_name libero_memory \
+  --dataset_name libero_stove \
   --run_root_dir outputs \
   --use_l1_regression False \
   --use_diffusion False \
   --use_film False \
-  --num_images_in_input 3 \
+  --num_images_in_input 20 \
   --use_proprio False \
-  --batch_size 4 \
+  --batch_size 1 \
   --grad_accumulation_steps 4 \
   --learning_rate 5e-4 \
   --num_steps_before_decay 100000 \
@@ -21,31 +22,4 @@ torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
   --image_aug True \
   --lora_rank 32 \
   --wandb_entity neuroking \
-  --wandb_project openvla-oft \
-  --run_id_note memorydisent
-
-
-# torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
-#   --disentangle none \
-#   --with_memory "[10000000, 1]"\
-#   --vla_path openvla/openvla-7b \
-#   --data_root_dir dataset/ \
-#   --dataset_name libero_memory \
-#   --run_root_dir outputs \
-#   --use_l1_regression False \
-#   --use_diffusion False \
-#   --use_film False \
-#   --num_images_in_input 1 \
-#   --use_proprio False \
-#   --batch_size 4 \
-#   --grad_accumulation_steps 4 \
-#   --learning_rate 5e-4 \
-#   --num_steps_before_decay 100000 \
-#   --max_steps 170005 \
-#   --save_freq 10000 \
-#   --save_latest_checkpoint_only False \
-#   --image_aug True \
-#   --lora_rank 32 \
-#   --wandb_entity neuroking \
-#   --wandb_project openvla-oft \
-#   --run_id_note memory
+  --wandb_project openvla-oft
